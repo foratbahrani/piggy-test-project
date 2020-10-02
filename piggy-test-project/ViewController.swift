2acfeeda81e09660a5d05ecf55c47579442c066d
Forat Bahrani <forybahrani@gmail.com>
2020-10-02 13:58:43 +0300

    Initial Commit

---
+++ piggy-test-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  piggy-test-project
+//
+//  Created by Forat Bahrani on ٢/١٠/٢٠٢٠.
+//  Copyright © 2020 Forat Bahrani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ piggy-test-projectTests/piggy_test_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  piggy_test_projectTests.swift
+//  piggy-test-projectTests
+//
+//  Created by Forat Bahrani on ٢/١٠/٢٠٢٠.
+//  Copyright © 2020 Forat Bahrani. All rights reserved.
+//
+
+import XCTest
+@testable import piggy_test_project
+
+class piggy_test_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
